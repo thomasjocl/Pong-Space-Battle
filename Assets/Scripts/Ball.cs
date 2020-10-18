2ffe27dbb65951d9e22e85c962fde527f27da00d
Thomas Cordova <tcordova@bancoripley.com.pe>
2020-10-17 19:33:40 -0500

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+     
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(5f, 5f);
+    }
+     
+    void Update()
+    {
+        
+    }
+}
