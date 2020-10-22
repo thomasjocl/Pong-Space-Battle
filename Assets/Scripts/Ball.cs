9c1bfe3cdae389096c866ef5b9c1fe5911cc4ff6
Thomas Cordova <tcordova@bancoripley.com.pe>
2020-10-22 18:34:18 -0500

    Power Ups

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,15 +3,41 @@
 public class Ball : MonoBehaviour
 {
     Rigidbody2D rb;
-     
+    public float speed;
+    public Camera cam;
+    Vector2 mousePos;
+    Vector2 lookDir;
+
+    public bool flagOriginal;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(5f, 5f);
     }
-     
+
     void Update()
-    {
-        
-    }
+    {   
+        var v = rb.velocity.normalized;
+        v *= speed;
+        rb.velocity = v;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && cam != null)
+        {
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            lookDir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+            rb.velocity = new Vector2(lookDir.x, lookDir.y).normalized * speed;
+            Debug.Log(rb.velocity);
+            Debug.Log(rb.velocity.magnitude);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            var balls = GameObject.FindGameObjectsWithTag("Ball");
+            foreach(var ball in balls)
+            {
+                if (!ball.GetComponent<Ball>().flagOriginal)
+                    Destroy(ball);
+            }
+        }
+    } 
 }
---
+++ Assets/Scripts/DuplicatePU.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class DuplicatePU : MonoBehaviour
+{
+    public GameObject ball;
+    ParticleSystem explosion;
+    SpriteRenderer sprite;
+    bool enable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion = GetComponent<ParticleSystem>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            enable = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+
+        var yVel = collision.gameObject.GetComponent<Rigidbody2D>().velocity.y;
+         
+        var xVel = Mathf.Atan(Random.Range(45, 135) * Mathf.Deg2Rad) * yVel;
+
+        if (enable)
+        {
+            var _vel = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+
+            var _ball = Instantiate(ball);
+
+            _ball.transform.position = collision.transform.position;
+
+            _ball.GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel * -1);
+        }
+
+        enable = false;
+
+        sprite.enabled = false;
+
+        explosion.Play(); 
+
+        Destroy(gameObject, explosion.main.duration);
+    }
+}
