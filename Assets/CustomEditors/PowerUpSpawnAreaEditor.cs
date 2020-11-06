db5098b634899e578e3f7fb1fbdc49bc607dd3e0
Thomas Cordova <tcordova@bancoripley.com.pe>
2020-11-06 03:31:40 -0500

    Editor innecesario pero para apreender

---
+++ Assets/CustomEditors/PowerUpSpawnAreaEditor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.CustomEditors
+{
+    [CustomEditor(typeof(PowerUpSpawnArea))]
+    [CanEditMultipleObjects]
+    public class PowerUpSpawnAreaEditor : Editor
+    {
+        int lastCount;
+        //int m_powerUpListSize;
+        SerializedProperty m_powerUps;
+        List<bool> m_PowerUpsFold;
+
+        private void OnEnable()
+        {
+            m_powerUps = serializedObject.FindProperty("powerUps");  
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_powerUps.FindPropertyRelative("Array.size"), new GUIContent("N° PowerUps"));
+
+            if (lastCount < m_powerUps.arraySize)
+            {
+                for (int e = 0; e < (m_powerUps.arraySize - lastCount); e++)
+                {
+                    m_powerUps.GetArrayElementAtIndex(lastCount + e).FindPropertyRelative("Prefab").objectReferenceValue = null;
+                    m_powerUps.GetArrayElementAtIndex(lastCount + e).FindPropertyRelative("Probability").floatValue = 0f;
+                }
+            }
+
+            lastCount = m_powerUps.arraySize;
+
+            for (int i = 0; i < m_powerUps.arraySize; i++)
+            {
+                if (m_PowerUpsFold.Count < m_powerUps.arraySize)
+                {
+                    m_PowerUpsFold.AddRange(Enumerable.Repeat(true, (m_powerUps.arraySize - m_PowerUpsFold.Count)));
+                }
+                else
+                    m_PowerUpsFold.RemoveRange(m_powerUps.arraySize, (m_PowerUpsFold.Count - m_powerUps.arraySize));
+
+                m_PowerUpsFold[i] = EditorGUILayout.Foldout(m_PowerUpsFold[i], "PowerUp " + (i + 1).ToString());
+
+                if (m_PowerUpsFold[i])
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.PropertyField(m_powerUps.GetArrayElementAtIndex(i).FindPropertyRelative("Prefab"));
+                    EditorGUILayout.Slider(m_powerUps.GetArrayElementAtIndex(i).FindPropertyRelative("Probability"), 0f, 20f);
+                    EditorGUI.indentLevel--;
+                }
+            } 
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUpSpawnArea.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawnArea : MonoBehaviour
+{
+    [SerializeField]
+    List<PowerUp> powerUps;
+
+    [SerializeField]
+    float minX, maxX, minY, maxY;
+
+    [SerializeField]
+    float respawnTime, initRespawnTime;
+
+    [SerializeField]
+    int maxPowersOnScreen;
+     
+    float nextTimeToSpawnPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextTimeToSpawnPower = Time.time + initRespawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int countPowersOnScreen = GameObject.FindGameObjectsWithTag("PowerUp").Length;
+        
+        if(countPowersOnScreen <= maxPowersOnScreen && Time.time > nextTimeToSpawnPower)
+        {
+        }
+
+        Debug.Log(countPowersOnScreen);
+    }
+}
+
+[Serializable]
+public class PowerUp
+{
+    public GameObject Prefab;
+
+    [Range(0f,100f)]
+    public float Probability;
+}
